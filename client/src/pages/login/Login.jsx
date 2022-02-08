41ef7cccd2b8b487bd3e82c815d86d2f6ffe1749
jeff k <sundev01@outlook.com>
2022-02-09 00:14:27 +0100

    Style login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,11 +1,17 @@
 import styles from "./login.module.css";
+import googleIcon from "../../images/googleIcon.png";
 
 function Login() {
   return (
-    <div>
-      <h1 className={styles.loginTitle}>Elige tu metodo de login</h1>
+    <div className={styles.login}>
       <div className={styles.wrapper}>
-        <div className={styles.left}></div>
+        <div className={styles.left}>
+          <h1 className={styles.loginTitle}>Log in to your account</h1>
+          <div className={styles.btnWrapper}>
+            <img src={googleIcon} alt="" className={styles.icon} />
+            <button className={styles.loginButton}>Log in with Google</button>
+          </div>
+        </div>
         <div className={styles.right}></div>
       </div>
     </div>
