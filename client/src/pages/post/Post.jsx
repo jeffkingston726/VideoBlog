3df3c201ea81caf57f77b1b2065cc3bc37acf97f
jeff k <sundev01@outlook.com>
2022-02-08 23:02:58 +0100

    Style post page

--- client/src/pages/post/Post.jsx
+++ client/src/pages/post/Post.jsx
@@ -1,7 +1,16 @@
 import styles from "./post.module.css";
+import { posts } from "../../data";
 
 function Post() {
-  return <div>post page</div>;
+  const post = posts[2];
+  return (
+    <div className={styles.post}>
+      <img src={post.img} alt="" className={styles.postImg} />
+      <h1 className={styles.postTitle}>{post.title}</h1>
+      <p className={styles.postDesc}>{post.desc}</p>
+      <p className={styles.postLongDesc}>{post.longDesc}</p>
+    </div>
+  );
 }
 
 export default Post;
