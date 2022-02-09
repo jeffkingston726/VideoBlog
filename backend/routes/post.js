1c17a76b081912c1cf1a8518033eda16040c7a49
jeff k <sundev01@outlook.com>
2022-02-09 04:05:11 +0100

    add endpoints for create, delete, update, getById, getAll

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -12,110 +12,62 @@ router.post("/", async (req, res) => {
   }
 });
 
-// //UPDATE CANDIDATE
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const candidate = await Candidate.findById(req.params.id);
-//     // if (candidate.username === req.body.username) {
-//     try {
-//       const updatedCandidate = await Candidate.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//           $set: req.body,
-//         },
-//         { new: true }
-//       );
-//       res.status(200).json(updatedCandidate);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//     // } else {
-//     //   res.status(401).json("You can update only your Candidates");
-//     // }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//UPDATE POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-// //DELETE CANDIDATE
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const candidate = await Candidate.findById(req.params.id);
-//     //if (candidate.username === req.body.username) {
-//     try {
-//       await candidate.delete();
-//       res.status(200).send("Candidate has been deleted...");
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//     // } else {
-//     //   res.status(401).json("You can delete only your candidates");
-//     // }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// //GET CANDIDATE
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const candidate = await Candidate.findById(req.params.id);
-//     res.status(200).json(candidate);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-// //GET ALL AND FILTER
-// router.get("/", async (req, res) => {
-//   const techName = req.query.tech;
-//   const countryName = req.query.country;
-//   const cityName = req.query.city;
-//   const attendanceName = req.query.attendance;
-//   const transferName = req.query.transfer;
-//   const employment = req.query.employment;
+    try {
+      await post.delete();
+      res.status(200).send("Post has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//   let splitTech;
-//   techName ? (splitTech = techName.split(",")) : null;
+//GET POST BY ID
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//   try {
-//     let candidate;
-//     if (
-//       techName &&
-//       countryName &&
-//       cityName &&
-//       attendanceName &&
-//       transferName &&
-//       employment
-//     ) {
-//       candidate = await Candidate.find({
-//         technologies: {
-//           $in: [...splitTech],
-//         },
-//         country: {
-//           $in: [countryName],
-//         },
-//         city: {
-//           $in: [cityName],
-//         },
-//         attendance: {
-//           $in: [attendanceName],
-//         },
-//         transfer: {
-//           $in: [transferName],
-//         },
-//         employment: {
-//           $in: [employment],
-//         },
-//       });
-//     } else {
-//       // Returns from newest to oldest.
-//       candidate = await Candidate.find().sort({ createdAt: -1 });
-//     }
-//     res.status(200).json(candidate);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
