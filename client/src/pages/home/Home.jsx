7b38e05c482b4755b6cf951daae9638de6898d4f
jeff k <sundev01@outlook.com>
2022-02-09 11:23:04 +0100

    Fetch post from mongoDB

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,25 @@
 import styles from "./home.module.css";
 import { posts } from "../../data";
 import Card from "../../components/card/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router";
 
 function Home() {
+  const [post, setPost] = useState(null);
+  const { location } = useLocation();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("http://localhost:5000/api/post");
+      setPost(res.data);
+    };
+    fetchPosts();
+  }, [location]);
+
+  console.log(post);
   return (
     <div className={styles.home}>
-      {posts.map(post => (
-        <Card key={post.id} post={post} />
-      ))}
+      {post && post.map(post => <Card key={post._id} post={post} />)}
     </div>
   );
 }
