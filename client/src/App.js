f93b6e5693d42c28da68afbc1801e7d820b79f19
jeff k <sundev01@outlook.com>
2022-02-09 01:05:58 +0100

    Add react-router-dom and created the routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,30 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Post from "./pages/post/Post";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      {/* <Post /> */}
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar user={user} />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,28 @@
 import styles from "./navbar.module.css";
 import avatarimg from "../../images/avatar.jpg";
+import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ user }) {
   return (
     <div className={styles.navbar}>
-      <span className={styles.logo}>Video Blog</span>
-      <ul className={styles.list}>
-        <li className={styles.listItem}>
-          <img src={avatarimg} alt="" className={styles.avatar} />
-        </li>
-        <li className={styles.listItem}>Aristides Cruz</li>
-        <li className={`${styles.listItem}`}>Logout</li>
-      </ul>
+      <span className={styles.logo}>
+        <Link className="link" to="/">
+          Video Blog
+        </Link>
+      </span>
+      {user ? (
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <img src={avatarimg} alt="" className={styles.avatar} />
+          </li>
+          <li className={styles.listItem}>Aristides Cruz</li>
+          <li className={`${styles.listItem}`}>Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      )}
     </div>
   );
 }
