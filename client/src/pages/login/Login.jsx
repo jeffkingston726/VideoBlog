35b71f374376a5236bd44fe42768133fe0817d78
jeff k <sundev01@outlook.com>
2022-02-09 03:23:11 +0100

    install packages (moongose, multer, path)

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,5 @@
 import styles from "./login.module.css";
 import googleIcon from "../../images/googleIcon.png";
-//import githubIcon from "../../images/github.png";
 
 function Login() {
   const google = async () => {
@@ -16,16 +15,6 @@ function Login() {
             <img src={googleIcon} alt="" className={styles.icon} />
             <button className={styles.loginButton}>Log in with Google</button>
           </div>
-          {/* <div className={styles.btnWrapper}>
-            <img
-              src={githubIcon}
-              alt=""
-              className={`${styles.icon} ${styles.githubIcon}`}
-            />
-            <button className={`${styles.loginButton} ${styles.github}`}>
-              Log in with Github
-            </button>
-          </div> */}
         </div>
         <div className={styles.right}></div>
       </div>
